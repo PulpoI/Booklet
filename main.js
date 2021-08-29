f042c753093b07da1bbee6f83b1a26d137fe2dad
Pulpoi <pablo37942814@gmail.com>
2021-08-29 00:24:46 -0300

    2

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 //Booklet 2 actividad 1
 
+let ingresaNombre = prompt("Ingresa un nombre")
+let nombre = "PABLO"
+if (ingresaNombre == nombre){
+    alert("Fui yo")
+}else{
+    alert("No fui yo")
+}
